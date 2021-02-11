96a45e9734090a6a8debe674e279abf9ec978b9b
Junshen Kevin Chen <kevinehc@gmail.com>
2021-02-11 11:18:04 -0800

    train.seen.issue.epoch

--- media_frame_transformer/12_train_seen_issue.py
+++ media_frame_transformer/12_train_seen_issue.py
@@ -6,10 +6,10 @@ from media_frame_transformer.dataset import load_kfold
 from media_frame_transformer.training import train
 from media_frame_transformer.utils import mkdir_overwrite
 
-EXPERIMENT_NAME = "1.2-a"
+EXPERIMENT_NAME = "1.2-c"
 
 KFOLD = 8
-N_EPOCH = 15
+N_EPOCH = 10
 
 if __name__ == "__main__":
     save_root = join(MODELS_DIR, EXPERIMENT_NAME)
