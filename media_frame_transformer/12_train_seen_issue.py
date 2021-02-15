f22cd7f9d0949d43fec9ed8fbc0e8debc46616f5
Junshen Kevin Chen <kevinehc@gmail.com>
2021-02-14 19:18:22 -0800

    12.newtrain

--- media_frame_transformer/12_train_seen_issue.py
+++ media_frame_transformer/12_train_seen_issue.py
@@ -1,28 +1,44 @@
+from os import mkdir
 from os.path import exists, join
 
 from config import ISSUES, MODELS_DIR
 
+from media_frame_transformer import models
 from media_frame_transformer.dataset import load_kfold
 from media_frame_transformer.learning import train
-from media_frame_transformer.utils import mkdir_overwrite
+from media_frame_transformer.utils import mkdir_overwrite, write_str_list_as_txt
 
-EXPERIMENT_NAME = "1.2-d"
+EXPERIMENT_NAME = "1.2-e"
+ARCH = "roberta_highdrop_half"
 
 KFOLD = 8
-N_EPOCH = 8
+N_EPOCH = 15
+BATCHSIZE = 30
 
 if __name__ == "__main__":
     save_root = join(MODELS_DIR, EXPERIMENT_NAME)
-    assert not exists(
-        save_root
-    ), f"{save_root} already exists, remove existing or choose another experment name"
-    mkdir_overwrite(save_root)
+    if not exists(save_root):
+        mkdir(save_root)
 
     kfold_datasets = load_kfold(ISSUES, "primary_frame", KFOLD)
     for ki, datasets in enumerate(kfold_datasets):
         save_fold = join(save_root, f"fold_{ki}")
+        if exists(join(save_fold, "_complete")):
+            print(">> skip", ki)
+            continue
         mkdir_overwrite(save_fold)
 
         train_dataset = datasets["train"]
         valid_dataset = datasets["valid"]
-        train(train_dataset, valid_dataset, save_fold, N_EPOCH)
+
+        model = models.get_model(ARCH)
+        train(
+            model,
+            train_dataset,
+            valid_dataset,
+            save_fold,
+            N_EPOCH,
+            BATCHSIZE,
+        )
+
+        write_str_list_as_txt(["."], join(save_fold, "_complete"))
