52d86f6cc5ff630572aad3da51383934379c74fa
Junshen Kevin Chen <kevinehc@gmail.com>
2021-02-25 01:11:59 -0800

    2111.2x

--- media_frame_transformer/21_11_aug_multi_spans_within_issue.py
+++ media_frame_transformer/21_11_aug_multi_spans_within_issue.py
@@ -18,11 +18,11 @@ from media_frame_transformer.eval import reduce_and_save_metrics
 from media_frame_transformer.learning import get_kfold_metrics, train
 from media_frame_transformer.utils import mkdir_overwrite, write_str_list_as_txt
 
-EXPERIMENT_NAME = "2.1.1.1.meddrop_half.w2.1x"
+EXPERIMENT_NAME = "2.1.1.1.meddrop_half.w2.2x"
 ARCH = "roberta_meddrop_half"
 
 AUG_WEIGHT = 0.2
-AUG_SET_SIZE_MULTIPLIER = 1
+AUG_SET_SIZE_MULTIPLIER = 2
 
 KFOLD = 8
 FOLDS_TO_RUN = [0, 1, 2]
