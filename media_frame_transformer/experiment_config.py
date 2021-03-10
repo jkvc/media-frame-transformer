48e834f86248c6f50f4a30fd6b9a65c2d7a20382
Junshen Kevin Chen <kevinehc@gmail.com>
2021-03-09 22:02:40 -0800

    12b

--- media_frame_transformer/experiment_config.py
+++ media_frame_transformer/experiment_config.py
@@ -5,8 +5,8 @@ G_CUDA_MEM = torch.cuda.get_device_properties(0).total_memory / 1024 / 1024 / 10
 KFOLD = 8
 FOLDS_TO_RUN = [0, 1, 2]
 
-# ARCH = "roberta_meddrop"
-# BATCHSIZE = 25
+ARCH = "roberta_meddrop"
+BATCHSIZE = 25
 
-ARCH = "roberta_meddrop_half"
-BATCHSIZE = 50
+# ARCH = "roberta_meddrop_half"
+# BATCHSIZE = 50
