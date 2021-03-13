78725405fbe7afc27e24dca6a29020dacec4f07e
Junshen Kevin Chen <kevinehc@gmail.com>
2021-03-12 23:04:17 -0800

    fold0123

--- media_frame_transformer/experiment_config.py
+++ media_frame_transformer/experiment_config.py
@@ -1,9 +1,7 @@
 import torch
 
-G_CUDA_MEM = torch.cuda.get_device_properties(0).total_memory / 1024 / 1024 / 1024
-
 KFOLD = 8
-FOLDS_TO_RUN = [0, 1, 2]
+FOLDS_TO_RUN = [0, 1, 2, 3]
 
 ARCH = "roberta_meddrop"
 BATCHSIZE = 25
