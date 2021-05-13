821b6ef716b2c2d00b5439a1e5e191797297ff9d
Junshen Kevin Chen <kevinehc@gmail.com>
2021-05-13 02:19:56 -0700

    lex.holdout.hyperparam

--- media_frame_transformer/1.lexicon_experiment/holdout_source_hyperparam.py
+++ media_frame_transformer/1.lexicon_experiment/holdout_source_hyperparam.py
@@ -1,4 +1,4 @@
-# Usage: python <script_name> <dataset_name>
+# Usage: python <script_name> <dataset_name> <logreg_arch>
 
 import sys
 from os.path import basename, join, realpath
@@ -13,27 +13,29 @@ from media_frame_transformer.model.logreg_config.grid_search import (
 
 _DATASET_NAME = sys.argv[1]
 _DATADEF = get_datadef(_DATASET_NAME)
+_ARCH = sys.argv[2]
+_CONFIG = load_logreg_model_config_all_archs(_DATADEF.n_classes, _DATADEF.n_sources)[
+    _ARCH
+]
 
 _SCRIPT_PATH = realpath(__file__)
 _EXPERIMENT_NAME = basename(_SCRIPT_PATH).replace(".py", "")
-_SAVE_ROOT = join(LEXICON_DIR, _DATASET_NAME, _EXPERIMENT_NAME)
+_SAVE_ROOT = join(LEXICON_DIR, _DATASET_NAME, _EXPERIMENT_NAME, _ARCH)
 
-_ARCH2CONFIG = load_logreg_model_config_all_archs(
-    _DATADEF.n_classes, _DATADEF.n_sources
-)
-for arch, config in _ARCH2CONFIG.items():
-    print("\n")
-    print("+" * 30)
-    print(arch)
-    print("+" * 30)
+_L1_REG_CANDIDATES = [1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
 
-    savedir = join(_SAVE_ROOT, arch)
+for reg in _L1_REG_CANDIDATES:
+    savedir = join(_SAVE_ROOT, str(reg))
+
+    config = {**_CONFIG}
+    config["reg"] = reg  # override
+
     for holdout_source in _DATADEF.source_names:
         print(">>", holdout_source)
         train_sources = [s for s in _DATADEF.source_names if s != holdout_source]
         train_samples = _DATADEF.load_splits_func(train_sources, ["train"])["train"]
         # valid using holdout issue all samples
-        valid_samples = _DATADEF.load_splits_func([holdout_source], ["train"])["train"]
+        valid_samples = _DATADEF.load_splits_func(train_sources, ["valid"])["valid"]
 
         run_lexicon_experiment(
             config,
@@ -43,7 +45,7 @@ for arch, config in _ARCH2CONFIG.items():
             vocab_size=config["vocab_size"],
             logdir=join(savedir, holdout_source),
             train_labelprop_split="train",
-            valid_labelprop_split="train",
+            valid_labelprop_split="valid",
         )
 
 reduce_and_save_metrics(_SAVE_ROOT)
